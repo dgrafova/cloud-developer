3ef7266ae89f84ec29dc1ab0e847a9da078b8b47
Daria Graf <d.graf@posteo.de>
2021-03-17 20:49:37 +0100

    added create and get items with dynamodb

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -4,9 +4,36 @@ import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } f
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
 
-export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  const newTodo: CreateTodoRequest = JSON.parse(event.body)
+const AWS = require('aws-sdk');
+const uuid = require('uuid');
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
+const grafTodoApp = process.env.TODO_TABLE;
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing incoming event: ', event);
+
+const todoId = uuid.v4();
+const newTodo: CreateTodoRequest = JSON.parse(event.body);
+
+const newItem = {
+  todoId: todoId,
+  ...newTodo
+}
+
+await dynamoDBClient.put({
+  TableName: grafTodoApp,
+  Item: newItem
+}).promise()
+
+return {
+  statusCode: 201,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
+  body: JSON.stringify({
+    newItem
+  })
+}
 }
--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,29 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+const AWS = require('aws-sdk');
+
+const dynamoDBClient = new AWS.DynamoDB.DocumentClient();
+const grafTodoApp = process.env.TODO_TABLE;
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+ 
+  console.log('Processing incoming event: ', event);
+
+  const result = await dynamoDBClient.scan({
+    TableName: grafTodoApp,
+  }).promise();
+
+  const items = result.Items;
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify({
+      items: items
+    })
+  }
 }
